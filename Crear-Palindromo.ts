a0089daf7cf81c984d80f7fec20ebb1eb90c9212
María <72228855+MariaRod123@users.noreply.github.com>
2023-01-22 23:51:22 -0300

    Update Crear-Palindromo.ts

--- Crear-Palindromo.ts
+++ Crear-Palindromo.ts
@@ -2,5 +2,5 @@ function crearPalindromo(cadenaTexto: string): string {
     let cadenaTextoInvertida = cadenaTexto.split('').reverse().join('');
     return cadenaTexto.concat(cadenaTextoInvertida);
 }
-console.log(crearPalindromo("prueba")); 
+console.log(crearPalindromo("prueba"));  
 
